671d7e0211ad4603d367fdb9baebaa08fad28f35
frcachon <fracachon@gmail.com>
2024-06-01 17:57:40 -0300

    improvements

--- README.md
+++ README.md
@@ -5,4 +5,14 @@
 
 - Instalar el módulo DeepFace usando `pip install deepface`.
 
-- Correr el script usando `python deepface_recognition.py`.
+- Correr el script usando `python deepface_recognition.py -i1 PATH_IMAGE_1 -i2 PATH_IMAGE_2`.
+
+#### Argumentos
+- Los argumentos `-i1 (--image1)` y `-i2 (--image2)` son requeridos.
+
+- El argumento `own_db` refiere a si las imágenes son ambas de la base de datos local, la cual, por su etiquetado actúa como groundtruth de la predicción. En caso de que se usen ambas imágenes de la db, es recomendable agregar al comando de ejecución: `--own_db True` así se podrá saber si la predicción realizada fue correcta o no. Este argumento tiene `False` como valor por defecto, por lo que sólo se mostrará el resultado de la predicción en consola.
+
+#### Ejemplo de uso
+Un posible comando a utilizar, con imagenes de la base de datos etiquetada es el siguiente:
+
+`python deepface_recognition.py -i1 ./database/Forlan2.jpg -i2 ./database/Cavani3.jpg --own_db True`
--- deepface_recognition.py
+++ deepface_recognition.py
@@ -8,30 +8,44 @@ db = './database'
 # Obtener todas las imagenes de la base de datos
 image_files = [f for f in os.listdir(db) if f.endswith('.jpg')]
 
-for _ in range(15):
-    # Seleccionar dos imagenes aleatorias
-    img1_path = random.choice(image_files)
-    img2_path = random.choice(image_files)
-
+def facial_recognition(image1: str, image2: str, own_db: bool) -> None:
     # Asegurarse que sean distintas
-    while img1_path == img2_path:
-        img2_path = random.choice(image_files)
+    if image1 == image2:
+        print("Las imagenes deben ser distintas.")
+        return
 
     # Compararlas usando DeepFace
     try:
-        result = DeepFace.verify(os.path.join(db, img1_path), os.path.join(db, img2_path))
+        result = DeepFace.verify(image1, image2)
     except:
-        continue
+        print("Error al comparar las imagenes.")
+        return
 
     # Mostrar los resultados
     print("Son la misma persona." if result["verified"] else "Persona distinta.")
 
     # Verificar si la prediccion fue correcta
-    if img1_path[:6] == img2_path[:6] and result["verified"] == True:
-        print("La prediccion fue correcta (está bien que haya dicho que sí son la misma persona).\n")
-    elif img1_path[:6] != img2_path[:6] and result["verified"] == False:
-        print("La prediccion fue correcta (está bien que haya dicho que no son la misma persona).\n")
-    elif img1_path[:6] == img2_path[:6] and result["verified"] == False:
-        print("La prediccion fue incorrecta (está mal que haya dicho que no son la misma persona).\n")
-    else:
-        print("La prediccion fue incorrecta (está mal que haya dicho que sí son la misma persona).\n")
+    if own_db:
+        if image1[-11:-5] == image2[-11:-5] and result["verified"] == True:
+            print("La prediccion fue correcta (está bien que haya dicho que sí son la misma persona).\n")
+        elif image1[-11:-5] != image2[-11:-5] and result["verified"] == False:
+            print("La prediccion fue correcta (está bien que haya dicho que no son la misma persona).\n")
+        elif image1[-11:-5] == image2[-11:-5] and result["verified"] == False:
+            print("La prediccion fue incorrecta (está mal que haya dicho que no son la misma persona).\n")
+        else:
+            print("La prediccion fue incorrecta (está mal que haya dicho que sí son la misma persona).\n")
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='DeepFace Recognition')
+    parser.add_argument('-i1', '--image1', required=True, type=str, help='Path de la primera imagen')
+    parser.add_argument('-i2','--image2', required=True, type=str, help='Path de la segunda imagen')
+    parser.add_argument('--own_db', type=bool, default=False, help='Path de la base de datos')
+    args = parser.parse_args()
+
+    img1_path = args.image1
+    img2_path = args.image2
+    own_db = args.own_db
+
+    facial_recognition(img1_path, img2_path, own_db)
